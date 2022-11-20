89916743f9e16931db8e8f12cf475a6260bb0101
Carlos <carlosdaniel.mc@hotmail.com>
2022-11-20 01:38:17 -0300

    s

---
+++ main/Program.java
@@ -0,0 +1,33 @@
+package main;
+
+
+import entites.Colections;
+
+public class Program {
+	public static void main(String[] args) {
+		Colections cl = new Colections();
+		
+		cl.RandomGenerate();
+		
+		System.out.println("Total pares: " + cl.totalPares(cl.a)); 
+		System.out.println("Maior valor: " + cl.maiorValor(cl.a)); 
+		System.out.println("Total iguais: " + cl.totalIguais(cl.a, cl.b)); 
+		System.out.print("Multiplicar valores: ");
+		cl.multiplicaArrays(cl.a,cl.b);
+		System.out.println("");
+		System.out.print("Soma Arrays: ");
+		cl.somaArrays(cl.a,cl.b);
+		cl.mostraDados(cl.a);
+		System.out.println(" ");
+		System.out.println("Busca: " + cl.buscaArray(cl.a, 5));
+		cl.busca(5);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
